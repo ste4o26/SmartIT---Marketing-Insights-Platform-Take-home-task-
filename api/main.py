1c1850f2c909006e22e5ac4043ce28f88638a170
Stefan Damyanov <ste4o99@gmail.com>
2026-06-13 19:29:29 +0300

    fix(api): Load env from environ

--- api/main.py
+++ api/main.py
@@ -1,6 +1,12 @@
+import dotenv
 import fastapi
+
 from api.constants import SERVICE_URI_PREFIX
 from api.routers import auth
+from api.routers import crypto
+
+dotenv.load_dotenv()
 
 app = fastapi.FastAPI()
 app.include_router(auth.router, prefix=f"/{SERVICE_URI_PREFIX}")
+app.include_router(crypto.router, prefix=f"/{SERVICE_URI_PREFIX}")
