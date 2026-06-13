92afe759a40720c294ff6c24e4689a7604f5eb58
Stefan Damyanov <ste4o99@gmail.com>
2026-06-13 13:41:56 +0300

    chore(api): Prefix router uri

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 import fastapi
+from api.constants import SERVICE_URI_PREFIX
 from api.routers import auth
 
 app = fastapi.FastAPI()
-app.include_router(auth.router)
+app.include_router(auth.router, prefix=f"/{SERVICE_URI_PREFIX}")
