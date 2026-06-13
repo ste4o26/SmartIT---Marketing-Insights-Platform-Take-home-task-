dbdda23d1f110125a0e0a7c50bcc0136c71a116a
Stefan Damyanov <ste4o99@gmail.com>
2026-06-13 19:23:56 +0300

    fix(market): Load env vars from environ

--- market_data/main.py
+++ market_data/main.py
@@ -1,6 +1,9 @@
+import dotenv
 import fastapi
+
 from market_data.constants import SERVICE_URI_PREFIX
 from market_data.routers import crypto
 
+dotenv.load_dotenv()
 app = fastapi.FastAPI()
 app.include_router(crypto.router, prefix=f"/{SERVICE_URI_PREFIX}")
