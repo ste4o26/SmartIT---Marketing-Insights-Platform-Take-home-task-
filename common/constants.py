9d9e8d7ffcc3bbba8418c9a0b46c111c45b25134
Stefan Damyanov <ste4o99@gmail.com>
2026-06-13 21:33:13 +0300

    refactor(common): Move out more common constants

--- common/constants.py
+++ common/constants.py
@@ -1,3 +1,12 @@
+import enum
 import re
 
 SYMBOL_PATTERN = re.compile(r"^[A-Z0-9]{3,20}$")
+
+DEFAULT_TOKEN_EXPIRY_SECONDS = "600"
+BEARER = "Bearer"
+
+
+class InternalService(enum.StrEnum):
+    API = "api-service"
+    MARKET_DATA = "market-data-service"
